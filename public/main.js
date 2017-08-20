838e67d6e069a763da37b6f969f4677ec5e02ba3
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-08-20 15:01:03 +0200

    :sparkles: A little bit of styling and design

--- public/main.js
+++ public/main.js
@@ -1,3 +1,13 @@
 /*
  * (C) 2017 💩💩💩
  */
+
+window.onload = function() {
+  Particles.init({
+    selector: '.background',
+    color: '#ffffff',
+    minDistance: 130,
+    speed: 0.3,
+    connectParticles: true
+  });
+};
